2b551b544ba48d092d767ee2d6e22e888aa00c1a
applepie-bp <33608946+applepie-bp@users.noreply.github.com>
2018-06-20 20:20:08 +0100

    Add files via upload

---
+++ Assets/spawnmap.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class spawnmap : MonoBehaviour
+{
+	public GameObject block1;
+	public int mapX = 40;
+	public int mapZ = 40;
+	tile[,] MAP;
+	private Vector3 Origin = Vector3.zero;
+	public float heightScalar = 10F; // the range of values that height can take.
+	public float zoomScalar = 10F; // how zoomed into perlin noise.
+	public float xOffset; // randomly adds or subtracts from x and z coordinates to change terrain each game.
+	public float zOffset; // otherwise corrdinates would always be the same
+
+	void Start()
+	{
+		// run in a seprate thread to increase performance, allowing for other processes to be done at the same time.
+		xOffset = Random.Range(-1.0f, 1.0f);
+		zOffset = Random.Range(-1.0f, 1.0f);
+		StartCoroutine(CreateMap());
+	}
+
+	IEnumerator CreateMap()
+	{
+		float height;
+		MAP = new tile[mapZ, mapX];
+		// cycle through each column of the grid
+		for (int x = 0; x < mapX; x += 1)
+		{
+			//cycle through each tile in row
+			for (int z = 0; z < mapZ; z += 1)
+			{
+				//vector addition of right(1,0,0) * by scalar, which enlarges 1 and so shifting further right, same for forward(0,0,1)
+				Vector3 pos = Origin + Vector3.right * x + Vector3.forward * z;
+				//generate height
+				//height = z/2 + x/2 // <- DOESN'T give decimal answers ... needs to be divided by 2f
+				//height = Mathf.Sin(z)+Mathf.Cos(x)*20f; // <- Test for floating point .. sin repeats not random enough 
+				height = 40f * Mathf.PerlinNoise((float)x/10f + xOffset, z/10f + zOffset);
+				Debug.Log(height);
+				//creates copy of block at pos, of type block1, with the parent being the unity object this class is in
+				MAP[x, z] = new tile(height, pos, block1, this.gameObject);
+			}
+		}
+		yield return null;
+	}
+}
